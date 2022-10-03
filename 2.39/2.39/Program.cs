60b0475902bd1996a6f1701244bc09e948462bcd
KelerArtyom <113889007+KelerArtyom@users.noreply.github.com>
2022-10-03 12:25:39 +0300

    Add files via upload

---
+++ 2.39/2.39/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        double hour, min, sec, ang;
+        Console.WriteLine("Введите количество часов:");
+        hour = int.Parse(Console.ReadLine());
+        if (hour >= 0 && hour <= 23)
+        {
+            Console.WriteLine("Введите количество минут:");
+            min = int.Parse(Console.ReadLine());
+            if (min >= 0 && hour <= 59) 
+            {
+                Console.WriteLine("Введите количество секунд:");
+                sec = int.Parse(Console.ReadLine());
+                if (sec >= 0 && sec <= 59) 
+                {
+                    ang = (hour * 15) + (min * 0.5) + (sec * 0.008);
+                    Console.WriteLine($"Угол равен {ang} градусам");
+                }
+                else
+                {
+                    Console.WriteLine("Неверные данные");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверные данные");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверные данные");
+        }
+    }
+}
+
